3a8366f576c616d3553211bb74711a09d3d854cd
jayson <92954227+jaysd5@users.noreply.github.com>
2023-09-11 08:42:17 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST', 'localhost');
-define('DB_Name', 'ticketing');
-define('USER', 'root');
-define('PASS', '157326');
+define('DB_Name', '');
+define('USER', '');
+define('PASS', '');
 
 try {
     $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_Name, USER, PASS);
